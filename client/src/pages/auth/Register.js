d832704a089e104d8541ee815366ec53ce0d8fc8
dhruti26 <106862356+dhruti26@users.noreply.github.com>
2023-09-11 23:04:41 +0530

    reusable form committed

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Form from '../../components/shared/form/Form'
 
 const Register = () => {
   return (
-    <div>
-        <h1>Register Page</h1>
+    <>
+    <div className="row g-0">
+      <div className="col-md-8 form-banner">
+         <img src="./assets/images/img2.webp" alt="registerImage" />  
+       </div>
+       <div className="col-md-4 form-container">
+         <Form formTitle={"Register Page"} submitBtn={"Register"} formType={'register'}/>
+        </div>
     </div>
+  </>
   )
 }
 
